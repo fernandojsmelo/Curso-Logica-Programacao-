e6522e3852e01f009d8482d3b199d4586f251a34
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-04-22 13:56:07 -0300

    Aula 73 e 74. Exercício Caixa Eletrônico

---
+++ C++/Exercicio_6_Função_Se_Caixa_Eletrônico.cpp
@@ -0,0 +1,125 @@
+//
+// Created by fernandomelo on 22/04/2026.
+//
+#include <iostream> //Entrada e saída de dados
+#include <locale.h> //Para trabalhar com acentos
+
+//Organização de pastas e poder usar o cout, cin e endl
+using namespace std;
+
+int main(){
+
+	//Biblioteca responsável pelos acentos nas letras e palavras
+	setlocale(LC_ALL, "Portuguese");
+
+	/*Exercício
+	Crie um algoritmo que simule um caixa eletrônico.
+	O algoritmo de solicitar ao usuário que ele digite o valor
+	que deseja sacar. Esse valor deve ser entre 2 e 2.000 reais.
+
+	Após isso, imprima na tela quantas notas de 2, 5, 10, 20, 50, 100 e 200 reais
+	 são necessárias para completar o valor do saque.
+
+	*/
+
+	int valor, duzentos, cem, cinquenta, vinte, dez, cinco, dois, resto;
+
+	cout << "Digite o valor a ser sacado (entre 2 e 2000): \n";
+	cin >> valor;
+
+	if (  valor < 2 or valor > 2000 ){
+
+		cout << "Valor inválido!\n";
+
+	}else{
+
+		duzentos = valor / 200; //Pegar a centena com a divisão inteira
+		cout << "Resultado 200: " << duzentos << "\n";
+
+		valor -= duzentos * 200; //Subtrai com centena retirando o resto do valor
+		cout << "Resultado Valor: " << valor << "\n";
+
+		cem = valor / 100;
+		valor -= cem * 100;
+
+		cinquenta = valor / 50;
+		valor -= cinquenta * 50;
+
+		vinte = valor / 20;
+		valor -= vinte * 20;
+
+		dez = valor / 10;
+		valor -= dez * 10;
+
+		cinco = valor / 5;
+		valor -= cinco * 5;
+
+		dois = valor / 2;
+		valor -= dois * 2;
+
+		resto = valor;
+
+
+			if ( resto >= 1 ){
+
+				cout << "Valor inválido!";
+
+			}else{
+
+				if ( duzentos > 0 ){
+
+					cout << "Nota(s) de 200: " << duzentos << "\n";
+
+				}
+
+				if ( cem > 0 ){
+
+					cout << "Nota(s) de 100: " << cem << "\n";
+
+				}
+
+				if ( cinquenta > 0 ){
+
+					cout << "Nota(s) de 50: " << cinquenta << "\n";
+
+				}
+
+				if ( vinte > 0 ){
+
+					cout << "Nota(s) de 20: " << vinte << "\n";
+
+				}
+
+				if ( dez > 0 ){
+
+					cout << "Nota(s) de 10: " << dez << "\n";
+
+				}
+
+				if ( cinco > 0 ){
+
+					cout << "Nota(s) de 5: " << cinco << "\n";
+
+				}
+
+				if ( dois > 0 ){
+
+					cout << "Nota(s) de 2: " << dois << "\n";
+
+				}
+
+			}
+
+
+
+
+	}
+
+
+
+	//Impede o fechamento da tela após execução do código para dar tempo de testarmos e ver o resultado
+	system("pause");
+
+	//Tipo de retorno da função main que é um inteiro
+	return 0;
+}
