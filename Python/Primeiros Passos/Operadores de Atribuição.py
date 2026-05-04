6c0d75a718fc853233d4b7badb14310d6ed8df92
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-04 16:18:17 -0300

    Python - Aula 190. Python - Operadores de Atribuição

---
+++ Python/Primeiros Passos/Operadores de Atribuição.py
@@ -0,0 +1,32 @@
+"""
+=  numero = 1          / Recebe
++= numero = numero + 1 / Incremento
+-= numero = numero - 1 / Decremento
+*= numero = numero * 1 / Multiplicando
+/= numero = numero / 1 / Divide
+%= numero = numero % 1 / Resto
+"""
+
+#Operador =
+numero = 10
+print(numero) #Resultado = 10
+
+#Operador +=
+numero += 1
+print(numero) #Resultado = 10 + 1 = 11
+
+#Operador -=
+numero -= 1
+print(numero) #Resultado = 11 - 1 = 10
+
+#Operador *=
+numero *= 2
+print(numero) #Resultado = 10 * 2 = 20
+
+#Operador /=
+numero /= 2
+print(numero) #Resultado = 20 / 2 = 10
+
+#Operador %=
+numero %= 3
+print(numero) #Resultado = 10 / 3 = 1
