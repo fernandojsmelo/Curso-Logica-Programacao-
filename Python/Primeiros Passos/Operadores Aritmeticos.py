c1977bf23694d7a364906a7b95546fa10a4d41ed
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-04 15:17:24 -0300

    Python - Aula 188. Python - Operadores Aritméticos

---
+++ Python/Primeiros Passos/Operadores Aritmeticos.py
@@ -0,0 +1,37 @@
+"""
++  -> Adição / Soma
+-  -> Subtração / Diminui
+*  -> Multiplicação / Realiza multiplicação
+/  -> Divisão / Divide
+// -> Divisão inteira realiza a divisão entre operandos
+      e a parte decimal de ambos os operandos
+%  -> Módulo / Retorna o resto da divisão
+** -> Exponenciação / Eleva um número em uma potência
+"""
+
+numero1 = 10
+numero2 = 3
+
+adicao = numero1 + numero2
+print("Resultado Adição:", adicao)
+print("César" + " " + "Fonseca")
+
+subtracao = numero1 - numero2
+print("Resultado Subtração:", subtracao)
+
+multiplicacao = numero1 * numero2
+print("Resultado Multiplicação:", multiplicacao)
+print(numero1 * "C")
+
+divisao = numero1 / numero2
+print("Resultado Divisão:", divisao)
+
+divisaoInteira = numero1 // numero2
+print("Resultado Divisão Inteira", divisaoInteira)
+
+modulo = numero1 % numero2
+print("Resultado Módulo:", modulo)
+
+exponenciacao = numero1 ** numero2
+print("Resultado Exponenciação:",exponenciacao)
+
