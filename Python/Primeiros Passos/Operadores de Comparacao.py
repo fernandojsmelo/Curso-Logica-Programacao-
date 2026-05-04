2e830751d7697111fbac8be24bb4e1690b8e184a
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-04 16:07:57 -0300

    Python - Aula 189. Python - Operadores de Comparação

---
+++ Python/Primeiros Passos/Operadores de Comparacao.py
@@ -0,0 +1,30 @@
+"""
+== -> Igual          : Verifica sem um valor é igual ao outro
+!= -> Diferente de   : Verifica se um valor é diferente do outro
+>  -> Maior que      : Verifica se um valor é maior que o outro
+>= -> Maior ou igual : Verifica se um valor é maior ou igual ao outro
+<  -> Menor que      : Verifica se um valor é menor que o outro
+<= -> Menor ou igual : Verifica se um valor é menor ou igual ao outro
+"""
+
+numero1 = 10
+numero2 = 10
+
+#if = se
+if numero1 == numero2:
+    print("Ambos os números são iguais")
+
+if numero1 != numero2:
+    print("Os números são diferentes")
+
+if numero1 > numero2:
+    print("Número 1 é maior que Número 2")
+
+if numero1 >= numero2:
+    print("Número 1 é maior ou Igual ao Número 2")
+
+if numero1 < numero2:
+    print("Número 1 é menor que Número 2")
+
+if numero1 <= numero2:
+    print("Número 1 é menor ou Igual ao Número 2")
