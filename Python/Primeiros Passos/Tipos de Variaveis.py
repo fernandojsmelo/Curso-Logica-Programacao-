e1412934f80cac2ef655300a4531ceeeded87858
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-04 15:00:45 -0300

    Python - Aula 187. Tipos de Variáveis

---
+++ Python/Primeiros Passos/Tipos de Variaveis.py
@@ -0,0 +1,20 @@
+"""
+str   = string / textos
+int   = inteiro / números inteiros 1, 2, 3...
+float = flutuante / Casas decimais 12.90, 21.40...
+bool  = boleano / Verdadeiro ou Falso
+list  = listas / listas de nomes, números e etc...
+"""
+
+print(type("Bryan"))
+print(type(5))
+print(type(12.90))
+print(type(True))
+
+letras = ["A", "B", "C"]
+print(type(letras))
+
+print("\n\n")
+print("Bryan", type("Bryan"))
+print(5, type(5))
+print(12.90, type(12.90))
