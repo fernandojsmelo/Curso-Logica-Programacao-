c9b8b96b64f2f46a955e33afbc93033c2c497a41
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-04 14:25:35 -0300

    Python - 185. Python - Primeiros Passos

---
+++ Python/Primeiros Passos/Primeiros Passos.py
@@ -0,0 +1,24 @@
+#Comentário simples significa que a linha
+#não vai ser executada / interpretada
+
+"""
+No Python, precisamos usar 3 aspas duplas
+assim o python entende que é um comentário
+e não interpreta quando executar
+"""
+
+nome = "Ana Paula"
+print(nome)
+
+nome = 23
+print(nome)
+
+nome = 25 + 12
+print(nome)
+
+nomeUsuario = "Amanda"; sobreNomeUsuario = "Souza"
+print(nomeUsuario + sobreNomeUsuario)
+print(nomeUsuario, sobreNomeUsuario)
+
+print("Nome:", nomeUsuario)
+print("Sobrenome:", sobreNomeUsuario)
