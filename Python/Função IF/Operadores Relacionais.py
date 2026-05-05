abd632d32f1106ab0984618c6171a399e081f77f
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-05 14:45:13 -0300

    Python - 193. Operadores Relacionais

---
+++ Python/Função IF/Operadores Relacionais.py
@@ -0,0 +1,48 @@
+"""
+Operador
+== Igual
+!= Diferente De
+> Maior que
+< Menor que
+>= Maior ou igual que
+<= Menor ou igual que
+"""
+
+
+letra = "b"
+
+#Operador = Igual
+#if = Se
+if letra == "a":
+    print("A letra é igual a Letra A")
+
+#Operador != Diferente De
+if letra != "a":
+    print("A letra é diferente de A")
+
+#Operador > Maior que
+numero1 = 20
+numero2 = 15
+
+if numero1 > numero2:
+    print("O número1 é maior que o número 2")
+
+if numero1 > numero2: print("O número1 é maior que o número 2")
+
+#Operador < Menor que
+n1 = 15
+n2 = 30
+
+if n1 < n2:
+    print("O número 2 é maior que o número 1")
+
+#Operador >= Maior ou igual que
+num1 = 50
+
+if num1 >= 20:
+    print("O num1 é maior ou Igual a 20 ")
+
+num2 = 10
+#Operador <= Menor ou igual que
+if num2 <= 15:
+    print("O num2 é menor ou igual a 15")
