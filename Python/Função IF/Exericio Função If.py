e9d26425bd7e2461b75f2d53bbc1bcb72517e59c
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-05 15:56:38 -0300

    Python - Exercício de programação 1: Exercício Função If

---
+++ Python/Função IF/Exericio Função If.py
@@ -0,0 +1,12 @@
+pontuacao = 90
+
+if pontuacao >= 90:
+    print("Sua classificação é A. Parabéns!")
+elif pontuacao >= 80 and pontuacao < 90:
+    print("Sua classificação é B. Muito bom!")
+elif pontuacao >= 70 and pontuacao < 80:
+    print("Sua classificação é C. Muito bom!")
+elif pontuacao >= 60 and pontuacao < 70:
+    print("Sua classificação é D. Precisa melhorar!")
+else:
+    print("Sua classificação é F. Estude mais!")
