261cf4c7140978ca2eaa0e46e4c5d265315ce0e0
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-05 15:24:34 -0300

    Python - 195. Condional If / Elif / Else

---
+++ Python/Função IF/Condicional If Elif Else.py
@@ -0,0 +1,28 @@
+n1 = 55
+n2 = 55
+
+#if = se
+if n1 > n2:
+    print("n1 é maior que n2")
+#elif = senão se
+elif n1 == n2:
+    print("n1 e n2 são iguais")
+
+#----------------------------------
+
+numero1 = 30
+numero2 = 10
+
+if numero1 > numero2:
+    print("numero1 é maior que numero2")
+elif numero1 == numero2:
+    print("numero1 e numero2 são IGUAIS Exemplo ")
+else:
+    print("Nem uma das condições atende ao critério")
+
+#-------------------------------
+
+num1 = 50
+num2 = 12
+
+print("num1 é maior") if num1 > num2 else print("num 2 é maior")
