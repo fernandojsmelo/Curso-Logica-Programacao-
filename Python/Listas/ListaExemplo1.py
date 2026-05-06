ad4193671d205b2385829fd6fd3fcda781f65fe6
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-06 11:10:08 -0300

    Python - Aula 200. Listas exemplo 1

---
+++ Python/Listas/ListaExemplo1.py
@@ -0,0 +1,35 @@
+listaNome = ["Amanda", "Clarisse", "Roger", "Allan"]
+
+print(listaNome)
+print(len(listaNome)) #len aprensa o tamanho da lista
+
+#---------------------------------------
+
+lista1 = ["A", "B", "C"]
+lista2 = [1, 2, 3, 4]
+lista3 = [True, False, False]
+lista4 = ["Roberta", 34, False]
+
+print(lista1)
+print(lista2)
+print(lista3)
+print(lista4)
+
+print("\n\n")
+#--------------------------------------
+
+#              0       1      2    3       4        5
+listaTeste = ["A", "Banana", "C", "D", "Elefante", "P"]
+
+print(listaTeste[0]) #Imprimindo a posição 0 da lista
+print(listaTeste[1]) #Imprimindo a posição 1 da lista
+print(listaTeste[2]) #Imprimindo a posição 2 da lista
+print(listaTeste[3]) #Imprimindo a posição 3 da lista
+print(listaTeste[4]) #Imprimindo a posição 4 da lista
+
+print(listaTeste[-1]) #Último item da lista
+print(listaTeste[1:3])
+print(listaTeste[:3]) #Imprimo os 3 primeiros
+print(listaTeste[2:]) #A partir do terceito item
+print(listaTeste[::2]) #Imprimos de dois em dois
+print(listaTeste[::-1]) #Imprimindo a lista de trás para frente
