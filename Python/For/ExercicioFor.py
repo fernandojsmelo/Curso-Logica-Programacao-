3a43f2e4742a51c1c64072cf87bacf70a818db28
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-06 09:58:02 -0300

    Python - Exercício de programação 3: Exercício For

---
+++ Python/For/ExercicioFor.py
@@ -0,0 +1,5 @@
+# 1. Use a função range para gerar números de 1 a 10
+# 2. Use um loop for para iterar sobre esses números
+for numero in range(1, 11):
+    # 3. Dentro do loop, imprima o número atual
+    print(numero)
