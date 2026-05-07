7fe05e3d0037e296d5b85a86afa744e9f3803599
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-07 09:12:05 -0300

    Python - Exercício de programação 5: Exercício Dicionário

---
+++ Python/Dicionarios/ExercicioDicionario.py
@@ -0,0 +1,11 @@
+
+#Criando um dicionário chamado animais
+animais = {
+    'cão': 'latido',
+    'gato': 'miado',
+    'vaca': 'mugido'
+}
+
+#imprimindo o som do 'cão'
+som = animais["cão"]
+print(som)
