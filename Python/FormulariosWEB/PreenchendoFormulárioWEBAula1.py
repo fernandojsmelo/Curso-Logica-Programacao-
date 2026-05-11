886f2b96ebe30630c9e4a730753b50aa9f520f10
Fernando José da Silva Melo <fernandojsmelo@gmail.com>
2026-05-11 09:54:31 -0300

    Python - Aula 240. Preenchendo Formulário WEB aula 1

---
+++ Python/FormulariosWEB/PreenchendoFormulárioWEBAula1.py
@@ -0,0 +1,69 @@
+from selenium import webdriver as opcoesSelenium
+from selenium.webdriver.common.by import By
+import pyautogui as tempoEspera
+
+from selenium.webdriver.support.select import Select
+
+navegador = opcoesSelenium.Chrome()
+
+navegador.get("https://form.jotform.com/221436066464051")
+
+#Tempo para o computador abrir o formulário
+tempoEspera.sleep(5)
+
+#Preenche o campo Nome
+navegador.find_element(By.NAME, "q3_nome[first]").send_keys("Ana Paula")
+
+#Tempo para o computador abrir o formulário
+tempoEspera.sleep(1)
+
+#Preenche o campo Sobrenome
+navegador.find_element(By.NAME, "q3_nome[last]").send_keys("Souza")
+
+#Tempo para o computador abrir o formulário
+tempoEspera.sleep(1)
+
+#Pree nche o campo Email
+navegador.find_element(By.NAME, "q4_email").send_keys("ana.paula@hotmail.com")
+
+#Tempo para o computador abrir o formulário
+tempoEspera.sleep(3)
+
+pegaDropDown = navegador.find_element(By.ID, "input_5")
+itemSelecionado = Select(pegaDropDown)
+itemSelecionado.select_by_index(2)
+
+#Tempo para o computador abrir o formulário
+tempoEspera.sleep(2)
+
+filho = "Não"
+
+#if = se
+if filho == "Sim":
+
+    #Marca a opção tem filho / SIM
+    navegador.find_element(By.ID, "label_input_6_0").click()
+
+else:
+
+    # Marca a opção tem filho / NÃO
+    navegador.find_element(By.ID, "label_input_6_1").click()
+
+
+#Tempo para o computador abrir o formulário
+tempoEspera.sleep(2)
+
+#Marca a opção cor AZUL
+navegador.find_element(By.ID, "label_input_7_0").click()
+
+#Tempo para o computador abrir o formulário
+tempoEspera.sleep(2)
+
+#Marca a opção cor VERDE
+navegador.find_element(By.ID, "label_input_7_5").click()
+
+#Marquei 5 estrelas
+navegador.find_element(By.XPATH, '//*[@id="input_8"]/div[5]').click()
+
+
+
